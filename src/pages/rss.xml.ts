73c51b3bd149a1a1b4296b1e4fd52533a64b20c5
Wtf Lmao <wtflmao@126.com>
2024-09-02 09:16:57 +0800

    fix: rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,9 +2,9 @@ import type { APIContext } from 'astro'
 import rss from '@astrojs/rss'
 import { site } from '@/config.json'
 import { getSortedPosts } from '@/utils/content'
-import sanitizeHtml from 'sanitize-html';
-import MarkdownIt from 'markdown-it';
-const parser = new MarkdownIt();
+import sanitizeHtml from 'sanitize-html'
+import MarkdownIt from 'markdown-it'
+const parser = new MarkdownIt()
 
 export async function GET(context: APIContext) {
   const sortedPosts = await getSortedPosts()
@@ -17,10 +17,11 @@ export async function GET(context: APIContext) {
       link: `/posts/${post.slug}`,
       title: post.data.title,
       pubDate: post.data.date,
-      description: post.data.summary,
-      content: sanitizeHtml(parser.render(post.body), {
-        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+      //description: post.data.summary,
+      description: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
       }),
+      //content: ,
     })),
     customData: `<language>${site.lang}</language>`,
   })
